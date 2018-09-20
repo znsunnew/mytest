33bf0cc7e23028ad67915577067135a72c4d3d90
ygx <349323198@qq.com>
2018-09-20 10:27:44 +0800

    diyi

---
+++ text/src/main/java/cn/itcast/pojo/test.java
@@ -0,0 +1,8 @@
+package cn.itcast.pojo;
+
+public class test {
+
+    public static void main(String[] args) {
+        System.out.println(11111);
+    }
+}
---
+++ yada/src/main/java/cn/itcast/entity/baga.java
@@ -0,0 +1,4 @@
+package cn.itcast.entity;
+
+public class baga {
+}
