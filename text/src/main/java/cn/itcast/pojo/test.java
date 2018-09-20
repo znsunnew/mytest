fab85ad029ff180c1af32de80d1c87c89084dc61
ygx <349323198@qq.com>
2018-09-20 10:46:09 +0800

    妈呀

--- text/src/main/java/cn/itcast/pojo/test.java
+++ text/src/main/java/cn/itcast/pojo/test.java
@@ -4,5 +4,7 @@ public class test {
 
     public static void main(String[] args) {
         System.out.println(11111);
+        System.out.println(22222);
+        System.out.println(44444);
     }
 }
